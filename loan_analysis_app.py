a03a917b13beb2f058a809333dcfa7e1cbe8148f
Nwabueze Ugoh <126014542+brentomagic@users.noreply.github.com>
2024-09-02 15:55:50 +0100

    Update loan_analysis_app.py

--- loan_analysis_app.py
+++ loan_analysis_app.py
@@ -261,7 +261,7 @@ fig3.add_annotation(
 
 # Adjust box opacity
 for trace in fig3.data:
-    trace.marker.opacity = 10
+    trace.marker.opacity = 1
 
 # Display the boxplot in Streamlit
 st.plotly_chart(fig3, use_container_width=True)
